239dd98eee71a5efb4d421a090465cbad72a268b
EduardoMeloDeOliveira <eduardo.moliveira.dev@hotmail.com>
2026-05-05 03:47:18 -0300

    chore: commit incial do projeto

---
+++ med-center-api-v1/med-center-api-v1/src/main/java/io/github/EduardoMeloDeOliveira/med_center_api_v1/MedCenterApiV1Application.java
@@ -0,0 +1,13 @@
+package io.github.EduardoMeloDeOliveira.med_center_api_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCenterApiV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedCenterApiV1Application.class, args);
+	}
+
+}
---
+++ med-center-api-v1/med-center-api-v1/src/test/java/io/github/EduardoMeloDeOliveira/med_center_api_v1/MedCenterApiV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.EduardoMeloDeOliveira.med_center_api_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedCenterApiV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
