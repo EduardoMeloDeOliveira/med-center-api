55a15739824ace228184b7508393704b7bd38592
EduardoMeloDeOliveira <eduardo.moliveira.dev@hotmail.com>
2026-05-05 04:00:36 -0300

    feat: cria o repositório do contexto de pacientes

---
+++ med-center-api-v1/med-center-api-v1/src/main/java/io/github/EduardoMeloDeOliveira/med_center_api_v1/domain/pacientes/repository/PacienteRepository.java
@@ -0,0 +1,7 @@
+package io.github.EduardoMeloDeOliveira.med_center_api_v1.domain.pacientes.repository;
+
+import io.github.EduardoMeloDeOliveira.med_center_api_v1.domain.pacientes.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente,Long> {
+}
